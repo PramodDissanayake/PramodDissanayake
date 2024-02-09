92688c9a1e1dde4910062fc5033a868fdfa467a2
PramodDissanayake <pramod.pradeeptha@gmail.com>
2024-02-09 04:47:59 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let type = new Typed('.changing-text', {
+    strings: ['Web-Developer', 'Youtuber', 'Wordpress Expert'], // Note: 'strings' instead of 'Strings'
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+});
